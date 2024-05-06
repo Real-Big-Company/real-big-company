d1d943d4d7beee6dd064d3cc9c37181e3ed4b654
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-05-07 10:36:03 +1200

    Add local symlinks for mounting layouts

--- local_setup.sh
+++ local_setup.sh
@@ -3,6 +3,7 @@
 # This file sets up symlinks locally so components can be used.
 # On CloudCannon, this will use Site Mounting.
 
-[ ! -e component-library ] && ln -s ../component-library component-library
+[ ! -e component-library ] && ln -s ../common/component-library component-library
+[ ! -e site/_includes/layouts ] && ln -s ../../../common/layouts site/_includes/layouts
 
 [ ! -e data-lake ] && ln -s ../data-lake/api data-lake
