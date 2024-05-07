9af8b8d0e710481d2def67647ec5ff54bcd77d39
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-05-08 11:16:19 +1200

    Only show syndicated news on the main website

--- site/pages/pages.11tydata.js
+++ site/pages/pages.11tydata.js
@@ -1,9 +1,9 @@
 const allReports = require("../../data-lake/reports/all/page-1.json");
-const allNews = require("../../data-lake/news/all/page-1.json");
+const syndicatedNews = require("../../data-lake/news/aggregate/syndicate/true/page-1.json");
 
 module.exports = {
   eleventyComputed: {
-    aggregateNews: allNews.values,
+    syndicatedNews: syndicatedNews.values,
     reports: allReports.values,
   },
 };
